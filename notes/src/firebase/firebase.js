3b60efb2251ef04a7f66b3e296596baae7aded3d
PameSegovia <114256835+PameSegovia@users.noreply.github.com>
2023-04-27 19:28:06 -0400

    Responsive Ok

--- notes/src/firebase/firebase.js
+++ notes/src/firebase/firebase.js
@@ -5,12 +5,12 @@ import {   getAuth, GoogleAuthProvider,signInWithPopup} from "firebase/auth";
 
 //App's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAhTIjpZUrrkM--js19j2jREwV2YPJoe0g",
-  authDomain: "notes-react-54a31.firebaseapp.com",
-  projectId: "notes-react-54a31",
-  storageBucket: "notes-react-54a31.appspot.com",
-  messagingSenderId: "729885784193",
-  appId: "1:729885784193:web:78ec4a6b3f3e1c88187580"
+  apiKey: "AIzaSyAMVYM_jXMM5eFmHKElNUeZQoZeaGR8lC4",
+  authDomain: "block-de-notas-con-react.firebaseapp.com",
+  projectId: "block-de-notas-con-react",
+  storageBucket: "block-de-notas-con-react.appspot.com",
+  messagingSenderId: "661457495783",
+  appId: "1:661457495783:web:92974a64e8e2b87aa2e442"
 }; 
 
 // Initialize Firebase
